64b229fd4e09dfad1650684f55da0f8f37d167be
家里的PC <574287254@qq.com>
2017-07-13 23:32:22 +0800

    创建设备测试软件

---
+++ DeviceApp/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace DeviceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.LED = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelSet = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_closecmd = new System.Windows.Forms.TextBox();
+            this.textBox_opencmd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "120.27.45.38";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "2525";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "userid:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "type:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(139, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 21);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "test";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(95, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "state:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "deviceid:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(139, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(180, 21);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(139, 131);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(180, 21);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(345, 12);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(399, 444);
+            this.textBox7.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "identity",
+            "set",
+            "get",
+            "response",
+            "upload"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 20);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "identity";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 47);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "启动";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // LED
+            // 
+            this.LED.Enabled = false;
+            this.LED.Location = new System.Drawing.Point(12, 277);
+            this.LED.Name = "LED";
+            this.LED.Size = new System.Drawing.Size(327, 179);
+            this.LED.TabIndex = 16;
+            this.LED.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelSet
+            // 
+            this.labelSet.AutoSize = true;
+            this.labelSet.Location = new System.Drawing.Point(343, 459);
+            this.labelSet.Name = "labelSet";
+            this.labelSet.Size = new System.Drawing.Size(29, 12);
+            this.labelSet.TabIndex = 17;
+            this.labelSet.Text = "Set:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(195, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "closecmd:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(56, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "opencmd:";
+            // 
+            // textBox_closecmd
+            // 
+            this.textBox_closecmd.Location = new System.Drawing.Point(261, 197);
+            this.textBox_closecmd.Name = "textBox_closecmd";
+            this.textBox_closecmd.Size = new System.Drawing.Size(58, 21);
+            this.textBox_closecmd.TabIndex = 19;
+            this.textBox_closecmd.Text = "close";
+            // 
+            // textBox_opencmd
+            // 
+            this.textBox_opencmd.Location = new System.Drawing.Point(118, 194);
+            this.textBox_opencmd.Name = "textBox_opencmd";
+            this.textBox_opencmd.Size = new System.Drawing.Size(58, 21);
+            this.textBox_opencmd.TabIndex = 18;
+            this.textBox_opencmd.Text = "open";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 479);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_closecmd);
+            this.Controls.Add(this.textBox_opencmd);
+            this.Controls.Add(this.labelSet);
+            this.Controls.Add(this.LED);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button LED;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelSet;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_closecmd;
+        private System.Windows.Forms.TextBox textBox_opencmd;
+    }
+}
+
---
+++ DeviceApp/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+using System.Threading;
+using System.Drawing;
+
+namespace DeviceApp
+{
+    public partial class Form1 : Form
+    {
+        byte[] bytes = new byte[1024];
+        IPEndPoint ip;
+        Socket server;
+        Thread recThread;
+        int num = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public void addText(string str)
+        {
+            textBox7.AppendText(str);     // 追加文本，并且使得光标定位到插入地方。
+            textBox7.ScrollToCaret();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            LED.BackColor = Color.Red;
+        }
+
+
+        private void RecMothed()
+        {
+            string str = string.Empty;
+            int recv = 0;
+            IPEndPoint sender1 = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender1);
+            server.Bind(Remote);
+            addText("已启动监听！\r\n\r\n");
+            while (true)
+            {
+                Thread.Sleep(10);
+                byte[] byt = new byte[1000];
+                recv = server.ReceiveFrom(byt, ref Remote);
+                addText(string.Format("Rec From:{0}\r\n", Remote));
+                str = Encoding.GetEncoding("GB2312").GetString(byt, 0, byt.Length);
+                addText("Msg:" + str + "\r\n\r\n");
+                str = str.Replace("\0", "");
+                DeviceHelper dh;
+                dh = GetJson(str);
+                if (dh.type == "set" || dh.type == "get")
+                {
+                    textBox6.Text = dh.deviceid;
+                    textBox5.Text = dh.state;
+                }
+                if (dh.type == "set")
+                {
+                    num++;
+                    labelSet.Text = num.ToString();
+                    //返回数据
+                    if (dh.state == textBox_opencmd.Text)
+                    {
+                        dh.state = "灯已经开了";
+                        LED.BackColor = Color.Green;
+                    }
+                    else
+                    if (dh.state == textBox_closecmd.Text)
+                    {
+                        dh.state = "灯已经关了";
+                        LED.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        dh.state = "已经响应指令："+ dh.state;
+                    }
+                    dh.type = "response";
+                    bytes = Encoding.GetEncoding("GB2312").GetBytes(SetJson(dh));
+                    server.SendTo(bytes, ip);
+                    addText(string.Format("To Sever:{0}\r\n\r\n", SetJson(dh)));
+                }
+                else
+                if (dh.type == "get")
+                {
+                    //返回数据
+                    if (LED.BackColor == Color.Green)
+                    {
+                        dh.state = "客厅灯现在开着呢！";
+                    }
+                    else
+                    if (LED.BackColor == Color.Red)
+                    {
+                        dh.state = "客厅灯现在关着呢！";
+                    }
+                    dh.type = "response";
+                    bytes = Encoding.GetEncoding("GB2312").GetBytes(SetJson(dh));
+                    server.SendTo(bytes, ip);
+                    addText(string.Format("To Sever:{0}\r\n\r\n", SetJson(dh)));
+                }
+            }
+        }
+        /// <summary>
+        /// 转对象到JSON字符串
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="deviceid"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public static string SetJson(string type, string userid, string deviceid, string state)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            DeviceHelper info = new DeviceHelper();
+            info.type = type;
+            info.userid = userid;
+            info.deviceid = deviceid;
+            info.state = state;
+            //转为json字符串
+            string dd = js.Serialize(info);
+            return dd;
+        }
+
+        public static string SetJson(DeviceHelper info)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            //转为json字符串
+            string dd = js.Serialize(info);
+            return dd;
+        }
+        /// <summary>
+        /// 转JSON字符串为对象
+        /// </summary>
+        /// <param name="src">json格式的字符串</param>
+        /// <returns></returns>
+        public static DeviceHelper GetJson(string src)
+        {
+            DeviceHelper inf;
+            try
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                inf = new DeviceHelper();
+                inf = js.Deserialize<DeviceHelper>(src);
+                return inf;
+            }
+            catch
+            {
+                inf = new DeviceHelper();
+                return inf;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //发送数据
+            try
+            {
+                DeviceHelper dh = new DeviceHelper();
+                dh.type = comboBox1.Text;
+                dh.userid = textBox3.Text;
+                dh.deviceid = textBox6.Text;
+                dh.state = textBox5.Text;
+                string str = SetJson(dh);
+                bytes = Encoding.GetEncoding("GB2312").GetBytes(str);
+                server.SendTo(bytes, ip);
+                addText(string.Format("To Sever:{0}\r\n\r\n", str));
+            }
+            catch (Exception ex)
+            {
+                addText(ex.ToString() + "\r\n\r\n");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = string.Empty;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (button3.Text == "停止")
+            {
+                server.Close();
+                recThread.Abort();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "启动")
+            {
+                button3.Text = "停止";
+                bytes = new byte[1024];
+                ip = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                recThread = new Thread(RecMothed);
+                recThread.Start();
+                button1.Enabled = true;
+                timer1.Enabled = true;
+                timer1.Interval = 200;
+                timer1.Start();
+            }
+            else
+            {
+                button3.Text = "启动";
+                addText("已停止服务！\r\n\r\n");
+                button1.Enabled = false;
+                recThread.Abort();
+                server.Close();
+                server.Dispose();
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 10000;
+            try
+            {
+                DeviceHelper dh = new DeviceHelper();
+                dh.type = "identity";
+                dh.userid = textBox3.Text;
+                dh.deviceid = "1";
+                dh.state = "";
+                string str = SetJson(dh);
+                bytes = Encoding.GetEncoding("GB2312").GetBytes(str);
+                server.SendTo(bytes, ip);
+                addText(string.Format("To Sever:{0}\r\n\r\n", str));
+            }
+            catch (Exception ex)
+            {
+                addText(ex.ToString() + "\r\n\r\n");
+            }
+        }
+    }
+
+    /// <summary>
+    /// 通信协议类
+    /// </summary>
+    public class DeviceHelper
+    {
+        public string type;//消息类型
+        public string userid;//用户ID
+        public string deviceid;//设备ID
+        public string state;   //设备状态
+    }
+}
